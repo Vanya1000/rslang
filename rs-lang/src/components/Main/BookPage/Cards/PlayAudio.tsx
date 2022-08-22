35512b5a32f5563bf92dd3e649ab773bc35ed72c
Vanya1000 <Vanya1000@yandex.ru>
2022-08-22 13:23:45 +0300

    fix: eliminated problem with load all audio on page

--- rs-lang/src/components/Main/BookPage/Cards/PlayAudio.tsx
+++ rs-lang/src/components/Main/BookPage/Cards/PlayAudio.tsx
@@ -16,46 +16,42 @@ const PlayAudio: React.FC<PlayAudioPropsType> = ({audioData}) => {
   const [isPlaying, setIsPlaying] = useState(false);
 
   const { word, firstSent, secondSent } = audioData;
-  const [audio] = useState([new Audio(word), new Audio(firstSent), new Audio(secondSent)]);
+  const [audio, setAudio] = useState<HTMLAudioElement[] | null>(null);
 
-  
+useEffect(() => {
+  let fn1: () => void;
+  let fn2: () => void;
+  let fn3: () => void;
+    if (audio) {
+      fn1 = () => audio[1].play();
+      fn2 = () => audio[2].play();
+      fn3 = () => setIsPlaying(false);
+      audio[0].addEventListener('ended', fn1);
+      audio[1].addEventListener('ended', fn2);
+      audio[2].addEventListener('ended', fn3);
+      audio[0].play();
+    }
+    return () => {
+      if (audio) {
+        audio.forEach((item) => item.pause());
+        audio[0].removeEventListener('ended', fn1);
+        audio[1].removeEventListener('ended', fn2);
+        audio[2].removeEventListener('ended', fn3);
+        setIsPlaying(false);
+      }
+    }
+  }, [audio]);
+
 
   useEffect(() => {
     if (isPlaying) {
-      audio[0].play();
+      setAudio([new Audio(word), new Audio(firstSent), new Audio(secondSent)]);
     }
     if (!isPlaying) {
-      audio[0].pause();
-      audio[1].pause();
-      audio[2].pause();
-    }
-    return () => {
-      audio[0].pause();
-      audio[1].pause();
-      audio[2].pause();
+      setAudio(null);
+      setIsPlaying(false);
     }
   }, [isPlaying]);
-
-  useEffect(() => {
-    const fn1 = function() {
-      audio[1].play();
-    }
-    const fn2 = function() {
-      audio[2].play();
-    }
-    const fn3 = function() {
-      setIsPlaying(false);
-    }
-    audio[0].addEventListener('ended', fn1);
-    audio[1].addEventListener('ended', fn2);
-    audio[2].addEventListener('ended', fn3)
-    return () => {
-      audio[0].removeEventListener('ended', fn1);
-      audio[1].removeEventListener('ended', fn2);
-      audio[2].removeEventListener('ended', fn3);
-      setIsPlaying(false);
-    };
-  }, []);
 
   return (
     <span>
