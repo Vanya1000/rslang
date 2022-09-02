06e14a62dc3c943b0c9cc7fd3c7190a459c6e90a
Maria Guchkova <mariaguchkova1@gmail.com>
2022-09-02 17:34:51 +0300

    fix: start game after page reload

--- rs-lang/src/components/Main/GamePage/SprintPage/SprintGroup.tsx
+++ rs-lang/src/components/Main/GamePage/SprintPage/SprintGroup.tsx
@@ -11,7 +11,7 @@ const SprintGroup = () => {
           making educated guesses about what is right and what is wrong.
         </div>
         <div className='level-button__wrapper'>
-          {[0, 1, 2, 3, 4, 5].map((index) => <GameGroupButton group={index} key={index}/>)}
+          {[0, 1, 2, 3, 4, 5].map((index) => <GameGroupButton game='sprint' group={index} key={index}/>)}
         </div>
       </div>
     </div>
