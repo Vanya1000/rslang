bb427cb75f1dc3ecee1be5d0ff75320992a2dea0
Maria Guchkova <mariaguchkova1@gmail.com>
2022-09-03 09:39:02 +0300

    fix: add additional words for answer options

--- rs-lang/src/components/Main/GamePage/common.ts
+++ rs-lang/src/components/Main/GamePage/common.ts
@@ -2,6 +2,8 @@ import { GameType, WordType } from "./../../../types/type";
 
 const baseUrl = process.env.REACT_APP_API_URL;
 
+export const additionalWords = ['приобретать', 'неловко', 'сторож', 'обмануть', 'отбить', 'ненависть', 'хижина', 'низший', 'поселить', 'новичок'];
+
 export const shuffle = <T>(array: T[]) => {
   const shuffled = array.slice().sort(() => Math.random() - 0.5);
   return shuffled;
